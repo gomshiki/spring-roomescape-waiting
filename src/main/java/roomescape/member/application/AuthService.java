ae6ee58952c87bc2c92df61a2d05c128de5c093f
gomshiki <gomshiki@gmail.com>
2024-07-08 16:27:21 +0900

    refactor: custom exception default message 적용
    - 공통된 메세지 내용 관리 용이를 위해 default message 적용

--- src/main/java/roomescape/member/application/AuthService.java
+++ src/main/java/roomescape/member/application/AuthService.java
@@ -19,32 +19,33 @@ public class AuthService {
         this.memberRepository = memberRepository;
     }
 
-    public TokenResponseDto createToken(final LoginMemberRequestDto loginMemberRequestDto) throws MemberNotFoundException {
+    public TokenResponseDto createToken(final LoginMemberRequestDto loginMemberRequestDto) {
         final Member loginMember = new Member(loginMemberRequestDto.getEmail(), loginMemberRequestDto.getPassword());
         checkMember(loginMember);
-        final Long foundId = memberRepository.findIdByEmail(loginMember.getEmail()).orElseThrow(
-                () -> new MemberNotFoundException("해당하는 회원 정보가 없습니다."));
+        final Long foundId = memberRepository
+                .findIdByEmailAndPassword(loginMember.getEmail(), loginMember.getPassword())
+                .orElseThrow(MemberNotFoundException::new);
 
         String accessToken = jwtTokenProvider.createToken(String.valueOf(foundId));
         return new TokenResponseDto(accessToken);
     }
 
-    public void checkMember(final Member member) throws MemberNotFoundException {
+    public void checkMember(final Member member) {
         final boolean existByEmailAndPassword = memberRepository.existsByEmailAndPassword(
                 member.getEmail(), member.getPassword());
         if (!existByEmailAndPassword) {
-            throw new MemberNotFoundException("해당하는 회원 정보가 없습니다.");
+            throw new MemberNotFoundException();
         }
     }
 
     public MemberResponseDto findMemberName(final String token) {
         final boolean isTokenExpired = jwtTokenProvider.validateToken(token);
         if (!isTokenExpired) {
-            throw new AuthorizationException("만료된 토큰입니다.");
+            throw new AuthorizationException();
         }
         final String id = jwtTokenProvider.extractMemberIdFromToken(token);
-        final String nameById = memberRepository.findNameById(Long.parseLong(id)).orElseThrow(
-                () -> new MemberNotFoundException("해당 회원 정보가 없습니다."));
+        final String nameById = memberRepository.findNameById(Long.parseLong(id))
+                .orElseThrow(MemberNotFoundException::new);
 
         return new MemberResponseDto(nameById);
     }
@@ -52,11 +53,11 @@ public class AuthService {
     public MemberResponseDto findMember(final String token) {
         final boolean isTokenValid = jwtTokenProvider.validateToken(token);
         if (!isTokenValid) {
-            throw new AuthorizationException("만료된 토큰입니다.");
+            throw new AuthorizationException();
         }
         final String id = jwtTokenProvider.extractMemberIdFromToken(token);
-        final Member foundMember = memberRepository.findById(Long.parseLong(id)).orElseThrow(
-                () -> new MemberNotFoundException("해당 회원 정보가 없습니다."));
+        final Member foundMember = memberRepository.findById(Long.parseLong(id))
+                .orElseThrow(MemberNotFoundException::new);
 
         return new MemberResponseDto(foundMember.getId(), foundMember.getName(), foundMember.getEmail(), foundMember.getRole());
     }
--- src/main/java/roomescape/member/infra/AuthorizationException.java
+++ src/main/java/roomescape/member/infra/AuthorizationException.java
@@ -1,6 +1,11 @@
 package roomescape.member.infra;
 
 public class AuthorizationException extends RuntimeException {
+    private static final String DEFAULT_MESSAGE = "유효하지 않은 토큰입니다.";
+
+    public AuthorizationException() {
+        super(DEFAULT_MESSAGE);
+    }
 
     public AuthorizationException(String message) {
         super(message);
